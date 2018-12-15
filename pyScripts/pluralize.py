4e7d8b64065bfab3523798e276bfb035f46d4e77
beratalp <beratalp@gmail.com>
2018-12-15 23:41:52 +0300

    updated pluralize

--- pyScripts/pluralize.py
+++ pyScripts/pluralize.py
@@ -2,13 +2,25 @@ import sys
 from textblob import TextBlob
 import ast
 import subprocess
+from nltk import WordNetLemmatizer
+
+
+
+def phraseplural(string):
+    wnl = WordNetLemmatizer()
+    lemma = wnl.lemmatize(str, "n")
+    if lemma is not str:
+        return True
+    else:
+        return False
+
+
 
 if len(sys.argv) > 0:
-
     data = ast.literal_eval(sys.argv[1])
     jsonArray = []
     for str in data:
-        if subprocess.Popen(["python3", "isPlural.py", str], stdout=subprocess.PIPE) == "false":
+        if not phraseplural(str):
             jsonDict = {"word": str,
                         "plural": TextBlob(str).words[0].pluralize()}
             jsonArray.append(jsonDict)
